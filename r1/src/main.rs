3700a248883134a989673f3f4f651a7b7b163157
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-03-03 19:21:35 +0000

    🍰

--- r1/src/main.rs
+++ r1/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "
+    Hello, world!
+    Where in the world are we going to do this 
+    "
+    );
+
+    add();
+
+    fn add() -> i32 {
+        let x = 50;
+        let y = 90;
+        let c = x + y;
+        return;
+    }
 }
